6b8d3b50ac322a7682058993835e7f6ae8e8a8c6
SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
2025-06-14 19:14:16 +0300

    Squashed commit of the following:
    commit ba51469fab3804e3ce5a4bf4d22373d3f974fa25
    Merge: 64fbf63e 7e72a13e
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Sat Jun 14 19:10:47 2025 +0300
    
        Merge branch 'main' into Server
    
    commit 7e72a13ed6fe2ed92a591a8cfb26fea5cddc9ca4
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Sat Jun 14 19:06:50 2025 +0300
    
        Squashed commit of the following:
    
        commit 64fbf63ed4670e51009d40c8ff8c08024ddfea61
        Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
        Date:   Sat Jun 14 19:05:01 2025 +0300
    
            Squashed commit of the following:
    
            commit 966c94833661e14dc5f4babdc280afaa51f591fa
            Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
            Date:   Sat Jun 14 19:03:43 2025 +0300
    
                Squashed commit of the following:
    
                commit 373bbbb70e07f89065bdd7e27117f7c23dacea6b
                Author: slaveiks69 <damn_p@vk.com>
                Date:   Sat Jun 14 01:10:22 2025 +0300
    
                    update
    
                commit 3224adc8c020d0f4d02b051cb0038296bfeac96b
                Author: slaveiks69 <damn_p@vk.com>
                Date:   Fri Jun 13 16:52:40 2025 +0300
    
                    EBANYTAYA PARASHA UPDATE
    
        commit 039fb49957d9f4a6a6a13a5267b4fc5636640709
        Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
        Date:   Sat Jun 14 19:01:27 2025 +0300
    
            movement improve
    
    commit 64fbf63ed4670e51009d40c8ff8c08024ddfea61
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Sat Jun 14 19:05:01 2025 +0300
    
        Squashed commit of the following:
    
        commit 966c94833661e14dc5f4babdc280afaa51f591fa
        Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
        Date:   Sat Jun 14 19:03:43 2025 +0300
    
            Squashed commit of the following:
    
            commit 373bbbb70e07f89065bdd7e27117f7c23dacea6b
            Author: slaveiks69 <damn_p@vk.com>
            Date:   Sat Jun 14 01:10:22 2025 +0300
    
                update
    
            commit 3224adc8c020d0f4d02b051cb0038296bfeac96b
            Author: slaveiks69 <damn_p@vk.com>
            Date:   Fri Jun 13 16:52:40 2025 +0300
    
                EBANYTAYA PARASHA UPDATE
    
    commit 966c94833661e14dc5f4babdc280afaa51f591fa
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Sat Jun 14 19:03:43 2025 +0300
    
        Squashed commit of the following:
    
        commit 373bbbb70e07f89065bdd7e27117f7c23dacea6b
        Author: slaveiks69 <damn_p@vk.com>
        Date:   Sat Jun 14 01:10:22 2025 +0300
    
            update
    
        commit 3224adc8c020d0f4d02b051cb0038296bfeac96b
        Author: slaveiks69 <damn_p@vk.com>
        Date:   Fri Jun 13 16:52:40 2025 +0300
    
            EBANYTAYA PARASHA UPDATE
    
    commit 039fb49957d9f4a6a6a13a5267b4fc5636640709
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Sat Jun 14 19:01:27 2025 +0300
    
        movement improve
    
    commit f9ba6c8e7376dd77af36d528607053805372d4cd
    Merge: f30c914c b9446e09
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Fri Jun 13 13:40:41 2025 +0300
    
        Merge branch 'Pavel-veTKA-BLUA'
    
    commit f30c914cbd7257be60bec6237ebc8672c58f8e79
    Author: SlaveBoy24 <72941490+SlaveBoy24@users.noreply.github.com>
    Date:   Fri Jun 13 10:32:04 2025 +0300
    
        server logic

---
+++ Top Down Shooter/Assets/Game Assets/Scripts/Server Logic/PhotonConnection.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class PhotonConnection : MonoBehaviourPunCallbacks
+{
+    public static PhotonConnection Instance;
+    [SerializeField] private bool _connectedToServer;
+    [SerializeField] private bool _connectedToLobby;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+            ConnectToServer();
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    private void ConnectToServer()
+    {
+        Debug.Log("Connecting");
+        PhotonNetwork.NickName = "dencha24";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        _connectedToServer = true;
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        _connectedToLobby = true;
+        Debug.Log("Connected to Lobby");
+    }
+
+    public bool IsConnected()
+    {
+        return (_connectedToServer && _connectedToLobby);
+    }
+
+}
---
+++ Top Down Shooter/Assets/Game Assets/Scripts/Server Logic/RoomManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+
+    public GameObject testCreateRoomBtn;
+    public GameObject testStartGameBtn;
+
+    private void Start()
+    {
+        StartCoroutine(LobbyInitialization());
+    }
+
+    private IEnumerator LobbyInitialization()
+    {
+        yield return new WaitUntil(()=>IsConnectedToServer());
+
+        testCreateRoomBtn.SetActive(true);
+    }
+
+    private bool IsConnectedToServer()
+    {
+        if (PhotonConnection.Instance)
+            return PhotonConnection.Instance.IsConnected();
+        else
+            return false;
+    }
+
+    public void CreateRoom()
+    {
+        base.OnJoinedLobby();
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = false;
+
+        string shortUniqueId = "";
+
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            string uniqueString = DateTime.UtcNow.ToString("yyyyMMddHHmmssfff") + Guid.NewGuid().ToString();
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(uniqueString));
+
+            // Convert hash to a shorter Base64 string
+            shortUniqueId = Convert.ToBase64String(hashBytes)
+                                    .TrimEnd('=')
+                                    .Replace('/', '_')
+                                    .Replace('+', '-')
+                                    .Substring(0, 8); // Adjust length as needed
+        }
+
+        PhotonNetwork.CreateRoom(shortUniqueId, roomOptions, null);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        testCreateRoomBtn.SetActive(false);
+        testStartGameBtn.SetActive(true);
+
+        Room currentRoom = PhotonNetwork.CurrentRoom;
+
+        Debug.Log($"i am {PhotonNetwork.NickName} - {PhotonNetwork.LocalPlayer.UserId}");
+        Debug.Log($"master's index {currentRoom.masterClientId}");
+        Debug.Log($"master is {currentRoom.Players[currentRoom.masterClientId].NickName} - {currentRoom.Players[currentRoom.masterClientId].UserId}");
+
+        Debug.Log($"Connected to Room {currentRoom.Name}");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        base.OnPlayerPropertiesUpdate(targetPlayer, changedProps);
+    }
+
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        base.OnRoomPropertiesUpdate(propertiesThatChanged);
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
