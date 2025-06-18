520cf26d8e1bc6a4cb5187fff7f1f2b05e784b3c
slaveiks69 <damn_p@vk.com>
2025-06-18 05:45:48 +0300

    inventory

---
+++ Top Down Shooter/Assets/Game Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public ItemType Type = ItemType.None;
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount != 0) return;
+        
+        GameObject dropped = eventData.pointerDrag;
+        Item itemBehaviour = dropped.GetComponent<Item>();
+
+        if ((itemBehaviour.item.Type != Type) && (Type != ItemType.None)) return;
+
+        itemBehaviour.ParentAfterDrag = transform;
+    }
+}
