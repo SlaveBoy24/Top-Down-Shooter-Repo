886320958cc4893149398a039f2f4c23656948d3
slaveiks69 <damn_p@vk.com>
2025-07-22 21:14:09 +0300

    aboba

--- Top Down Shooter/Assets/Game Assets/Scripts/Weapon/Weapon Controller.cs
+++ Top Down Shooter/Assets/Game Assets/Scripts/Weapon/Weapon Controller.cs
@@ -4,9 +4,32 @@ public class WeaponController : MonoBehaviour
 {
     public GameObject WeaponHandler;
     private Animator _animator;
+    public Item InHandWeapon;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+    }
+
+    public void Shoot()
+    {
+        _animator.SetTrigger("shoot");
+        
+    }
+
+    public void ChangeState(string state)
+    {
+        switch (state)
+        {
+            case "rifle":
+                _animator.SetTrigger("rifle");
+                break;
+            case "pistol":
+                _animator.SetTrigger("pistol");
+                break;
+            default:
+                _animator.SetTrigger("unequip");
+                break;
+        }
     }
 }
