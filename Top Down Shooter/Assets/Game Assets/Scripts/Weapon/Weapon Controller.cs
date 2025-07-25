cd8bece08f5ddc5bc214efa0d7d4682022cd694f
slaveiks69 <damn_p@vk.com>
2025-07-25 20:04:02 +0300

    test

--- Top Down Shooter/Assets/Game Assets/Scripts/Weapon/Weapon Controller.cs
+++ Top Down Shooter/Assets/Game Assets/Scripts/Weapon/Weapon Controller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
@@ -5,7 +6,8 @@ public class WeaponController : MonoBehaviour
     public GameObject WeaponHandler;
     private Animator _animator;
     public Item InHandWeapon;
-
+    public GameObject ShootPrefab;
+    public Vector3 testOffset;
     private void Start()
     {
         _animator = GetComponent<Animator>();
@@ -13,10 +15,30 @@ public class WeaponController : MonoBehaviour
 
     public void Shoot()
     {
-        _animator.SetTrigger("shoot");
-        
+        StartCoroutine(OneShoot());
     }
+    private IEnumerator OneShoot()
+    {
+        float bulletSpeed = 40f;
 
+        _animator.SetTrigger("shoot");
+
+        var bullet = Instantiate(ShootPrefab);
+
+        bullet.transform.position = this.transform.position + testOffset; // for offset
+
+        bullet.transform.LookAt(this.gameObject.transform.position + testOffset + transform.forward);
+
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+
+        bulletRigidbody.AddForce(bullet.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(bullet);
+
+        yield break;
+    }
     public void ChangeState(string state)
     {
         switch (state)
